7d0618fcb378541de4a2e6ac55ea24368dfa13fc
Naruyoko <naru519372@gmail.com>
2019-07-18 16:21:17 -0400

    File export

--- interpreter/handler.js
+++ interpreter/handler.js
@@ -9,12 +9,17 @@ window.onload=function(){
     alert(error);
     console.error(error);
   }
+  changeRawProgram();
+  changeFileType();
+  generateFile();
 }
 
 var file=null;
 var reader=new FileReader();
 var rawProgram="";
 var hexPorgram="";
+var rawExtentions=[".pdl",".padl",".padlang"];
+var hexExtentions=[".pdlh",".padlh",".padlangh",".padh",".padhex",".padlanghex"];
 function onupload(){
   file=dg("upload").files[0];
 }
@@ -26,8 +31,14 @@ function readFile(){
   reader.readAsText(file,dg("encode").value);
 }
 reader.onload=function(e){
-  rawProgram=reader.result;
-  hexProgram=rawToHex(rawProgram);
+  var extention=file.name.substring(file.name.lastIndexOf("."));
+  if (!hexExtentions.includes(extention)){
+    rawProgram=reader.result;
+    hexProgram=rawToHex(rawProgram);
+  }else{
+    hexProgram=reader.result;
+    rawProgram=hexToRaw(hexProgram);
+  }
   displayProgram();
 }
 function displayProgram(){
@@ -44,4 +55,32 @@ function changeHexProgram(){
   hexProgram=dg("hexview").value;
   dg("rawview").value=rawProgram=hexToRaw(hexProgram);
   displayProgram();
+}
+
+function changeFileType(){
+  var extentions;
+  if (dg("fileType").value=="Raw"){
+    extentions=rawExtentions;
+  }else{
+    extentions=hexExtentions;
+  }
+  var s="";
+  for (var i=0;i<extentions.length;i++){
+    s+="<option value=\""+extentions[i]+"\">"+extentions[i]+"</option>";
+  }
+  dg("fileExtention").innerHTML=s;
+}
+//function from https://stackoverflow.com/a/29339233
+function download(text, name, type) {
+  var a = document.getElementById("saveLink");
+  var file = new Blob([text], {type: type});
+  a.href = URL.createObjectURL(file);
+  a.download = name;
+}
+function generateFile(){
+  if (dg("fileType").value=="Raw"){
+    download(rawProgram,dg("fileName").value+dg("fileExtention").value,"text/plain");
+  }else{
+    download(hexProgram,dg("fileName").value+dg("fileExtention").value,"text/plain");
+  }
 }
